9d180d837468b7603d9b8cd1bf82beec6008df65
Sandor Desi <d.alex.galaxy@gmail.com>
2022-09-15 07:29:59 +0100

    add animated logo

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,14 +1,29 @@
 import { Fragment } from "react";
+import Lottie from "react-lottie";
 import { Outlet, Link } from "react-router-dom";
+import animationData from "../../assets/little_cottage_.json";
 
 import "./navigation.styles.scss";
 
 const Navigation = () => {
+  const defaultOptions = {
+    loop: false,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {},
+  };
+
   return (
     <Fragment>
       <div className="navigation">
         <Link className="logo-container" to="/">
-          Logo
+          <Lottie
+            to="/"
+            style={{ margin: "0px" }}
+            options={defaultOptions}
+            height={100}
+            width={300}
+          />
         </Link>
         <div className="nav-links-container">
           <Link className="nav-link" to="/menu">
